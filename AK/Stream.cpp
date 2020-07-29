e217075b5c6ac9dc2b70a6fb07c2d1f09b22c337
asynts <asynts@gmail.com>
2020-07-29 12:08:34 +0200

    [WIP] AK: Add more operator<< overloads in 'Stream.h'.

--- AK/Stream.cpp
+++ AK/Stream.cpp
@@ -1,8 +1,11 @@
 #include "Stream.h"
 
 #include <AK/ByteBuffer.h>
+#include <AK/FlyString.h>
 #include <AK/String.h>
 #include <AK/StringView.h>
+
+#include <cstdio>
 
 OutputStream& OutputStream::operator<<(const String& value)
 {
@@ -17,4 +20,16 @@ OutputStream& OutputStream::operator<<(const StringView& value)
 OutputStream& OutputStream::operator<<(const ByteBuffer& value)
 {
     return *this << value.span();
+}
+
+OutputStream& OutputStream::operator<<(const FlyString& value)
+{
+    return *this << value.bytes();
+}
+
+OutputStream& OutputStream::operator<<(const void* value)
+{
+    char buffer[32];
+    sprintf(buffer, "%p", value);
+    return *this << buffer;
 }
