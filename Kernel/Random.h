ba48ca590a564780a2aac585747ba1e5aa7a80a6
asynts <asynts@gmail.com>
2020-08-15 18:38:24 +0200

    AK: Rename span() to bytes() when apropriate.
    I originally defined the bytes() method for the String class, because it
    made it obvious that it's a span of bytes instead of span of characters.
    
    This commit makes this more consistent by defining a bytes() method when
    the type of the span is known to be u8.
    
    Additionaly, the cast operator to Bytes is overloaded for ByteBuffer and
    such.

--- Kernel/Random.h
+++ Kernel/Random.h
@@ -69,7 +69,7 @@ public:
         typename CipherType::CTRMode cipher(m_key, KeySize);
 
         Bytes buffer_span { buffer, n };
-        auto counter_span = m_counter.span();
+        auto counter_span = m_counter.bytes();
         cipher.key_stream(buffer_span, counter_span, &counter_span);
 
         // Extract a new key from the prng stream.
