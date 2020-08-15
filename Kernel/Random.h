79af1e18640d24bb8b0b7a6e3ea91d74f1b13693
asynts <asynts@gmail.com>
2020-08-15 18:06:16 +0200

    Kernel: Take advantage of the operator Bytes cast.

--- Kernel/Random.h
+++ Kernel/Random.h
@@ -68,13 +68,11 @@ public:
 
         typename CipherType::CTRMode cipher(m_key, KeySize);
 
-        Bytes buffer_span { buffer, n };
         auto counter_span = m_counter.bytes();
-        cipher.key_stream(buffer_span, counter_span, &counter_span);
+        cipher.key_stream({ buffer, n }, counter_span, &counter_span);
 
         // Extract a new key from the prng stream.
-        Bytes key_span = m_key.span();
-        cipher.key_stream(key_span, counter_span, &counter_span);
+        cipher.key_stream(m_key, counter_span, &counter_span);
     }
 
     template<typename T>
