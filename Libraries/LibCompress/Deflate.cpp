7baaa9179d8b84edfadf03f74d5e854441361a53
asynts <asynts@gmail.com>
2020-08-23 13:56:55 +0200

    [WIP] Moving decompress_huffman_block into a class.
    Why doesn't the binary_search overload work?

--- Libraries/LibCompress/Deflate.cpp
+++ Libraries/LibCompress/Deflate.cpp
@@ -25,6 +25,7 @@
  */
 
 #include <AK/Assertions.h>
+#include <AK/BinarySearch.h>
 #include <AK/LogStream.h>
 #include <AK/Span.h>
 #include <AK/Types.h>
@@ -34,6 +35,11 @@
 #include <cstring>
 
 namespace Compress {
+
+bool CompressedBlock::read_next_chunk()
+{
+    TODO();
+}
 
 Vector<u8> Deflate::decompress()
 {
@@ -341,37 +347,15 @@ CanonicalCode::CanonicalCode(ReadonlyBytes codes)
     }
 }
 
-static i32 binary_search(Vector<u32>& heystack, u32 needle)
+u32 CanonicalCode::read_symbol(InputBitStream& stream) const
 {
-    i32 low = 0;
-    i32 high = heystack.size();
-
-    while (low <= high) {
-        u32 mid = (low + high) >> 1;
-        u32 value = heystack.at(mid);
-
-        if (value < needle) {
-            low = mid + 1;
-        } else if (value > needle) {
-            high = mid - 1;
-        } else {
-            return mid;
-        }
-    }
-
-    return -1;
-}
-
-u32 CanonicalCode::read_symbol(InputBitStream& stream)
-{
-    auto code_bits = 1;
+    u32 code_bits = 1;
 
     for (;;) {
-        code_bits = code_bits << 1 | stream.read_bit();
-        i32 index = binary_search(m_symbol_codes, code_bits);
-        if (index >= 0) {
-            return m_symbol_values.at(index);
-        }
+        code_bits = code_bits << 1 | stream.read_bits(1);
+        const auto index = binary_search(m_symbol_codes.span(), code_bits);
+        if (index >= 0)
+            return m_symbol_values[index];
     }
 }
 }
