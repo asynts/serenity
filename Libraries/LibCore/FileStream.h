63d7d541b63852cc3731b2c4da608fe289c1bab3
asynts <asynts@gmail.com>
2020-08-28 11:55:22 +0200

    LibCore: Add InputFileStream and OutputFileStream.

---
+++ Libraries/LibCore/FileStream.h
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2020, the SerenityOS developers.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/Stream.h>
+#include <LibCore/File.h>
+
+namespace Core {
+
+// FIXME: Buffering, maybe add a Buffered<T> template?
+class InputFileStream final : public InputStream {
+public:
+    explicit InputFileStream(NonnullRefPtr<File> file)
+        : m_file(file)
+    {
+    }
+
+    size_t read(Bytes bytes) override
+    {
+        size_t nread = 0;
+        while (nread < bytes.size()) {
+            if (m_file->eof())
+                return nread;
+
+            const auto buffer = m_file->read(nread - bytes.size());
+            nread += buffer.bytes().copy_to(bytes.slice(nread));
+        }
+
+        return nread;
+    }
+
+    bool read_or_error(Bytes bytes) override
+    {
+        if (read(bytes) < bytes.size()) {
+            m_error = true;
+            return false;
+        }
+
+        return true;
+    }
+
+    bool discard_or_error(size_t count) override
+    {
+        u8 buffer[4096];
+
+        size_t ndiscarded = 0;
+        while (ndiscarded < count) {
+            if (eof()) {
+                m_error = true;
+                return false;
+            }
+
+            ndiscarded += read({ buffer, min<size_t>(count - ndiscarded, sizeof(buffer)) });
+        }
+
+        return true;
+    }
+
+    bool eof() const override { return m_file->eof(); }
+
+private:
+    InputFileStream() = default;
+
+    NonnullRefPtr<File> m_file;
+};
+
+// FIXME: Buffering, maybe add a Buffered<T> template?
+class OutputFileStream : public OutputStream {
+public:
+    OutputFileStream(NonnullRefPtr<File> file)
+        : m_file(file)
+    {
+    }
+
+    size_t write(ReadonlyBytes bytes) override
+    {
+        if (!m_file->write(bytes.data(), bytes.size())) {
+            m_error = true;
+            return 0;
+        }
+
+        return bytes.size();
+    }
+
+    bool write_or_error(ReadonlyBytes bytes) override
+    {
+        if (write(bytes) < bytes.size()) {
+            m_error = true;
+            return false;
+        }
+
+        return true;
+    }
+
+private:
+    NonnullRefPtr<File> m_file;
+};
+
+}
