1af6e7463cb25916fa09a27316b051b503d99e40
asynts <asynts@gmail.com>
2020-08-28 12:14:05 +0200

    Fix bug in FileStream.h.

--- Userland/uncompress.cpp
+++ Userland/uncompress.cpp
@@ -29,41 +29,8 @@
 #include <LibCore/File.h>
 #include <LibCore/FileStream.h>
 
-StringView output_filename_for(StringView filename)
-{
-    return filename.substring_view(0, filename.length() - 3);
-}
-
-void decompress_file(Core::InputFileStream input_stream, Core::OutputFileStream output_stream)
-{
-    auto gzip_stream = Compress::GzipDecompressor { input_stream };
-
-    u8 buffer[4096];
-    while (!gzip_stream.eof()) {
-        ASSERT(!input_stream.has_error());
-        ASSERT(!output_stream.has_error());
-        ASSERT(!gzip_stream.has_error());
-
-        const auto nread = gzip_stream.read({ buffer, sizeof(buffer) });
-        output_stream.write({ buffer, nread });
-    }
-}
-
 int main(int argc, char** argv)
 {
-    Vector<const char*> filenames;
-
     Core::ArgsParser args_parser;
-    args_parser.add_positional_argument(filenames, "Path to compressed file.", "FILE");
     args_parser.parse(argc, argv);
-
-    for (StringView input_filename : filenames) {
-        ASSERT(input_filename.ends_with(".gz"));
-        const auto output_filename = input_filename.substring_view(0, input_filename.length() - 3);
-
-        auto input_file_result = Core::File::open(input_filename, Core::IODevice::OpenMode::ReadOnly);
-        auto output_file_result = Core::File::open(output_filename, Core::IODevice::OpenMode::WriteOnly);
-
-        decompress_file(Core::InputFileStream { input_file_result.value() }, Core::OutputFileStream { output_file_result.value() });
-    }
 }
