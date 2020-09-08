32d767e95d7bba15e3889eb11a8263ddf75fc685
asynts <asynts@gmail.com>
2020-09-08 18:28:15 +0200

    [WIP] Port util-linux's hexdump to serenity.

---
+++ Ports/hexdump/package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash ../.port_include.sh
+port=hexdump
+version=2.36
+useconfigure="true"
+files="https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v${version}/util-linux-${version}.tar.gz util-linux-${version}.tar.gz"
+configopts="--target=i686-pc-serenity"
+workdir="util-linux-${version}"
+
+build() {
+    run make $makeopts hexdump
+}
+
+install() {
+    run install hexdump "$SERENITY_ROOT/Build/Root/usr/local/bin/hexdump"
+}
+
+export CFLAGS="-Wno-deprecated-declarations"
