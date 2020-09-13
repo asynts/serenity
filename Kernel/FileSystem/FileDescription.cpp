928512f35c822ad8eddb8ae58377dd54febf4824
asynts <asynts@gmail.com>
2020-09-13 17:56:03 +0200

    FileSystem: Use FixedOutputMemoryStream instead of BufferStream.

--- Kernel/FileSystem/FileDescription.cpp
+++ Kernel/FileSystem/FileDescription.cpp
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <AK/BufferStream.h>
+#include <AK/MemoryStream.h>
 #include <Kernel/Devices/BlockDevice.h>
 #include <Kernel/Devices/CharacterDevice.h>
 #include <Kernel/FileSystem/Custody.h>
@@ -178,25 +178,24 @@ ssize_t FileDescription::get_dir_entries(u8* buffer, ssize_t size)
     size_t size_to_allocate = max(static_cast<size_t>(PAGE_SIZE), static_cast<size_t>(metadata.size));
 
     auto temp_buffer = ByteBuffer::create_uninitialized(size_to_allocate);
-    BufferStream stream(temp_buffer);
+    FixedOutputMemoryStream stream { temp_buffer };
+
     KResult result = VFS::the().traverse_directory_inode(*m_inode, [&stream, this](auto& entry) {
         stream << (u32)entry.inode.index();
         stream << m_inode->fs().internal_file_type_to_directory_entry_type(entry);
         stream << (u32)entry.name.length();
-        stream << entry.name;
+        stream << entry.name.bytes();
         return true;
     });
 
     if (result.is_error())
         return result;
 
-    stream.snip();
-
     if (static_cast<size_t>(size) < temp_buffer.size())
         return -EINVAL;
 
-    copy_to_user(buffer, temp_buffer.data(), temp_buffer.size());
-    return stream.offset();
+    copy_to_user(buffer, stream.data(), stream.size());
+    return stream.size();
 }
 
 bool FileDescription::is_device() const
