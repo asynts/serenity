4461c8f531bc11acfb8559f5d5e443d88f34b957
asynts <asynts@gmail.com>
2020-09-21 11:50:02 +0200

    [WIP] LibCompress: Add a constructor for CanonicalCode.

--- Libraries/LibCompress/CanonicalCode.h
+++ Libraries/LibCompress/CanonicalCode.h
@@ -31,71 +31,58 @@
 #include <AK/Optional.h>
 #include <AK/StdLibExtras.h>
 
-// #define DEBUG_CANONICAL_CODE
-
 namespace Compress::New {
 
+// FIXME: Put a large circular buffer into this class.
 class Reader {
 public:
     // Padded with zeroes if not enough bits are avaliable.
-    u16 peek() const
-    {
-        try_to_buffer_16bits();
-        return m_buffered & 0xffff;
-    }
+    u16 peek() const;
 
-    void consume(size_t bits)
-    {
-        try_to_buffer_16bits();
-#ifdef DEBUG_CANONICAL_CODE
-        ASSERT(bits <= m_buffered_bits);
-#endif
-        m_buffered_bits -= min(bits, m_buffered_bits);
-    }
-
-private:
-    size_t try_read_two_bytes(u16& value);
-
-    void try_to_buffer_16bits() const
-    {
-        // FIXME: Verify byte / bit order.
-
-        if (m_buffered_bits >= 16)
-            return;
-
-        if (m_stream.remaining() >= 2) {
-            u16 value;
-            m_stream >> value;
-
-            m_buffered |= static_cast<u32>(value) << m_buffered_bits;
-            m_buffered_bits += 2;
-        } else if (m_stream.remaining() == 1) {
-            u8 value;
-            m_stream >> value;
-
-            m_buffered |= static_cast<u32>(value) << m_buffered_bits;
-            m_buffered_bits += 1;
-        }
-    }
-
-    // FIXME: This obviously can't stay this way. The logic for this should be added to this stream.
-    mutable InputMemoryStream& m_stream;
-
-    mutable u32 m_buffered;
-    mutable size_t m_buffered_bits { 0 };
+    void consume(size_t bits);
 };
 
 // This is very much inspired by "Efficient Huffman Decoding" in https://www.hanshq.net/zip.html.
 class CanonicalCode {
 public:
+    // FIXME: Analyse the behavior of this constructor for invalid symbol lengths. Ensure that
+    //        whatever the output is, doesn't cause any crashes.
+    explicit CanonicalCode(Span<const u8> symbol_lengths)
+    {
+        Array<u8, 15> lengths { 0 };
+
+        for (auto length : symbol_lengths) {
+            ++lengths[length - 1];
+        }
+
+        u16 first_unused_code_word = 0;
+        for (size_t length_index = 0; length_index < 15; ++length_index) {
+            if (lengths[length_index] == 0)
+                continue;
+
+            m_max_length_index = length_index;
+
+            u16 first_symbol = 0;
+            for (; first_symbol < symbol_lengths.size(); ++first_symbol) {
+                if (symbol_lengths[first_symbol] == length_index)
+                    break;
+            }
+
+            m_first_codeword_offsets[length_index] = static_cast<i16>(first_symbol) - static_cast<i16>(first_unused_code_word);
+            m_sentinel_bits[length_index] = first_unused_code_word + lengths[length_index];
+
+            first_unused_code_word += lengths[length_index];
+        }
+    }
+
     u16 read_symbol()
     {
         const auto bits = m_reader.peek();
 
-        for (size_t idx = 0; idx < min<size_t>(m_bits_used, 15); ++idx) {
-            if (bits < m_sentinel_bits[idx]) {
-                m_reader.consume(idx + 1);
-                return m_first_codeword_offsets[idx] + bits;
+        for (size_t length_index = 0; length_index <= min<size_t>(m_max_length_index, 14); ++length_index) {
+            if (bits < m_sentinel_bits[length_index]) {
+                m_reader.consume(length_index + 1);
+                return m_first_codeword_offsets[length_index] + bits;
             }
         }
 
@@ -106,7 +93,7 @@ private:
     Reader m_reader;
     Array<u16, 15> m_sentinel_bits;
     Array<i16, 15> m_first_codeword_offsets;
-    u8 m_bits_used;
+    u8 m_max_length_index;
 };
 
 }
