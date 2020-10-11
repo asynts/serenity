238bd3483328a9c866ee49d63e7155d2317217eb
asynts <asynts@gmail.com>
2020-10-11 16:18:24 +0200

    [WIP] IPCCompiler: Use new SourceGenerator class.

--- DevTools/IPCCompiler/main.cpp
+++ DevTools/IPCCompiler/main.cpp
@@ -27,6 +27,7 @@
 #include <AK/Function.h>
 #include <AK/GenericLexer.h>
 #include <AK/HashMap.h>
+#include <AK/SourceGenerator.h>
 #include <AK/StringBuilder.h>
 #include <LibCore/File.h>
 #include <ctype.h>
@@ -199,38 +200,48 @@ int main(int argc, char** argv)
     while (lexer.tell() < file_contents.size())
         parse_endpoint();
 
-    out() << "#pragma once";
-    out() << "#include <AK/MemoryStream.h>";
-    out() << "#include <AK/OwnPtr.h>";
-    out() << "#include <AK/URL.h>";
-    out() << "#include <AK/Utf8View.h>";
-    out() << "#include <LibGfx/Color.h>";
-    out() << "#include <LibGfx/Rect.h>";
-    out() << "#include <LibGfx/ShareableBitmap.h>";
-    out() << "#include <LibIPC/Decoder.h>";
-    out() << "#include <LibIPC/Dictionary.h>";
-    out() << "#include <LibIPC/Encoder.h>";
-    out() << "#include <LibIPC/Endpoint.h>";
-    out() << "#include <LibIPC/Message.h>";
-    out();
+    SourceGenerator generator;
+
+    generator.append(R"~~~(
+    #pragma once
+    #include <AK/MemoryStream.h>
+    #include <AK/OwnPtr.h>
+    #include <AK/URL.h>
+    #include <AK/Utf8View.h>
+    #include <LibGfx/Color.h>
+    #include <LibGfx/Rect.h>
+    #include <LibGfx/ShareableBitmap.h>
+    #include <LibIPC/Decoder.h>
+    #include <LibIPC/Dictionary.h>
+    #include <LibIPC/Encoder.h>
+    #include <LibIPC/Endpoint.h>
+    #include <LibIPC/Message.h>
+)~~~");
 
     for (auto& endpoint : endpoints) {
-        out() << "namespace Messages::" << endpoint.name << " {";
-        out();
+        generator.set("endpoint.name", endpoint.name);
+        generator.set("endpoint.magic", String::number(endpoint.magic));
+
+        generator.append_pattern("namespace Messages::@endpoint.name@ {\n");
 
         HashMap<String, int> message_ids;
 
-        out() << "enum class MessageID : i32 {";
+        generator.append_pattern("enum class MessageID : i32 {");
         for (auto& message : endpoint.messages) {
             message_ids.set(message.name, message_ids.size() + 1);
-            out() << "    " << message.name << " = " << message_ids.size() << ",";
+            generator.set("message.name", message.name);
+            generator.set("message.id", String::number(message_ids.size() + 1));
+
+            generator.append_pattern("    @message.name@ = @message.id@,\n");
             if (message.is_synchronous) {
                 message_ids.set(message.response_name(), message_ids.size() + 1);
-                out() << "    " << message.response_name() << " = " << message_ids.size() << ",";
+                generator.set("message.name", message.response_name());
+                generator.set("message.id", String::number(message_ids.size() + 1));
+
+                generator.append_pattern("    @message.name@ = @message.id@,\n");
             }
         }
-        out() << "};";
-        out();
+        generator.append("};\n");
 
         auto constructor_for_message = [&](const String& name, const Vector<Parameter>& parameters) {
             StringBuilder builder;
@@ -267,31 +278,52 @@ int main(int argc, char** argv)
         };
 
         auto do_message = [&](const String& name, const Vector<Parameter>& parameters, const String& response_type = {}) {
-            out() << "class " << name << " final : public IPC::Message {";
-            out() << "public:";
-            if (!response_type.is_null())
-                out() << "    typedef class " << response_type << " ResponseType;";
-            out() << "    " << constructor_for_message(name, parameters);
-            out() << "    virtual ~" << name << "() override {}";
-            out() << "    virtual i32 endpoint_magic() const override { return " << endpoint.magic << "; }";
-            out() << "    virtual i32 message_id() const override { return (int)MessageID::" << name << "; }";
-            out() << "    static i32 static_message_id() { return (int)MessageID::" << name << "; }";
-            out() << "    virtual const char* message_name() const override { return \"" << endpoint.name << "::" << name << "\"; }";
-            out() << "    static OwnPtr<" << name << "> decode(InputMemoryStream& stream, size_t& size_in_bytes)";
-            out() << "    {";
+            generator.set("message.name", name);
+            generator.set("message.response_type", response_type);
 
-            out() << "        IPC::Decoder decoder(stream);";
+            generator.append_pattern(R"~~~(
+class @message.name@ final : public IPC::Message {
+public:
+)~~~");
+
+            if (!response_type.is_null())
+                generator.append_pattern(R"~~~(
+   typedef class @message.response_type@ ResponseType;
+)~~~");
+
+            generator.append_pattern(R"~~~(
+    @message.constructor@
+    virtual ~@message.name@() override {}
+
+    virtual i32 endpoint_magic() const override { return @endpoint.magic@; }
+    virtual i32 message_id() const override { return (int)MessageID::@message.name@; }
+    static i32 static_message_id() { return (int)MessageID::@message.name@; }
+    virtual const char* message_name() const override { return "@endpoint.name@::@message.name@"; }
+
+    static OwnPtr<@message.name@> decode(InputMemoryStream& stream, size_t& size_in_bytes)
+    {
+        IPC::Decoder decoder {stream};
+)~~~");
 
             for (auto& parameter : parameters) {
-                String initial_value = "{}";
+                generator.set("parameter.type", parameter.type);
+
                 if (parameter.type == "bool")
-                    initial_value = "false";
-                out() << "        " << parameter.type << " " << parameter.name << " = " << initial_value << ";";
-                out() << "        if (!decoder.decode(" << parameter.name << "))";
-                out() << "            return nullptr;";
+                    generator.set("parameter.initial_value", "false");
+                else
+                    generator.set("parameter.initial_value", "{}");
+
+                generator.append_pattern(R"~~~(
+        @parameter.type@ @parameter.name@ = @parameter.initial_value@;
+        if (!decoder.decode(@parameter.name@))
+            return nullptr;
+)~~~");
+
                 if (parameter.attributes.contains_slow("UTF8")) {
-                    out() << "        if (!Utf8View(" << parameter.name << ").validate())";
-                    out() << "            return nullptr;";
+                    generator.append_pattern(R"~~~(
+        if (!Utf8View(@parameter.name@).validate())
+            return nullptr;
+)~~~");
                 }
             }
 
@@ -302,29 +334,59 @@ int main(int argc, char** argv)
                 if (i != parameters.size() - 1)
                     builder.append(", ");
             }
-            out() << "        size_in_bytes = stream.offset();";
-            out() << "        return make<" << name << ">(" << builder.to_string() << ");";
-            out() << "    }";
-            out() << "    virtual IPC::MessageBuffer encode() const override";
-            out() << "    {";
-            out() << "        IPC::MessageBuffer buffer;";
-            out() << "        IPC::Encoder stream(buffer);";
-            out() << "        stream << endpoint_magic();";
-            out() << "        stream << (int)MessageID::" << name << ";";
+
+            generator.set("message.constructor_call_parameters", builder.build());
+
+            generator.append_pattern(R"~~~(
+        size_in_bytes = stream.offset();
+        return make<@message.name@>(@message.constructor_call_parameters);
+    }
+)~~~");
+
+            generator.append_pattern(R"~~~(
+    virtual IPC::MessageBuffer encode() const override
+    {
+        IPC::MessageBuffer buffer;
+        IPC::Encoder stream(buffer);
+        stream << endpoint_magic();
+        stream << (int)MessageID::@message.name@;
+)~~~");
+
             for (auto& parameter : parameters) {
-                out() << "        stream << m_" << parameter.name << ";";
+                generator.set("parameter.name", parameter.name);
+                generator.append_pattern(R"~~~(
+        stream << m_@parameter.name@;
+)~~~");
             }
-            out() << "        return buffer;";
-            out() << "    }";
+
+            generator.append_pattern(R"~~~(
+        return buffer;
+    }
+)~~~");
+
             for (auto& parameter : parameters) {
-                out() << "    const " << parameter.type << "& " << parameter.name << "() const { return m_" << parameter.name << "; }";
+                generator.set("parameter.type", parameter.type);
+                generator.set("parameter.name", parameter.name);
+                generator.append_pattern(R"~~~(
+    const @parameter.type@& @parameter.name@() const { return m_@parameter.name@; }
+)~~~");
             }
-            out() << "private:";
+
+            generator.append(R"~~~(
+private:
+            )~~~");
+
             for (auto& parameter : parameters) {
-                out() << "    " << parameter.type << " m_" << parameter.name << ";";
+                generator.set("parameter.type", parameter.type);
+                generator.set("parameter.name", parameter.name);
+                generator.append_pattern(R"~~~(
+    @parameter.type@ m_@parameter.name@;
+)~~~");
             }
-            out() << "};";
-            out();
+
+            generator.append(R"~~~(
+};
+            )~~~");
         };
         for (auto& message : endpoint.messages) {
             String response_name;
@@ -334,75 +396,118 @@ int main(int argc, char** argv)
             }
             do_message(message.name, message.inputs, response_name);
         }
-        out() << "}";
-        out();
 
-        out() << "class " << endpoint.name << "Endpoint : public IPC::Endpoint {";
-        out() << "public:";
-        out() << "    " << endpoint.name << "Endpoint() {}";
-        out() << "    virtual ~" << endpoint.name << "Endpoint() override {}";
-        out() << "    static int static_magic() { return " << endpoint.magic << "; }";
-        out() << "    virtual int magic() const override { return " << endpoint.magic << "; }";
-        out() << "    static String static_name() { return \"" << endpoint.name << "\"; };";
-        out() << "    virtual String name() const override { return \"" << endpoint.name << "\"; };";
-        out() << "    static OwnPtr<IPC::Message> decode_message(const ByteBuffer& buffer, size_t& size_in_bytes)";
-        out() << "    {";
-        out() << "        InputMemoryStream stream { buffer };";
-        out() << "        i32 message_endpoint_magic = 0;";
-        out() << "        stream >> message_endpoint_magic;";
-        out() << "        if (stream.handle_any_error()) {";
+        generator.append_pattern(R"~~~(
+} // namespace Messages::@endpoint.name@
+        )~~~");
+
+        generator.append_pattern(R"~~~(
+class @endpoint.name@Endpoint : public IPC::Endpoint {
+public:
+    @endpoint.name@Endpoint() { }
+    virtual ~@endpoint.name@Endpoint() override { }";
+
+    static int static_magic() { return @endpoint.magic@; }
+    virtual int magic() const override { return @endpoint.magic@; }
+    virtual String name() const override { return "@endpoint.name@"; };
+
+    static OwnPtr<IPC::Message> decode_message(const ByteBuffer& buffer, size_t& size_in_bytes)
+    {
+        InputMemoryStream stream { buffer };
+        i32 message_endpoint_magic = 0;
+        stream >> message_endpoint_magic;
+        if (stream.handle_any_error()) {
+)~~~");
 #ifdef GENERATE_DEBUG_CODE
-        out() << "            dbg() << \"Failed to read message endpoint magic\";";
+        generator.append_pattern(R"~~~(
+            dbgln("Failed to read message endpoint magic");
+)~~~");
 #endif
-        out() << "            return nullptr;";
-        out() << "        }";
-        out() << "        if (message_endpoint_magic != " << endpoint.magic << ") {";
+        generator.append_pattern(R"~~~(
+            return nullptr;
+        }
+
+        if (message_endpoint_magic != @endpoint.magic@) {
+)~~~");
 #ifdef GENERATE_DEBUG_CODE
-        out() << "            dbg() << \"endpoint magic \" << message_endpoint_magic << \" != " << endpoint.magic << "\";";
+        generator.append_pattern(R"~~~(
+            dbgln("Endpoint magic number message_endpoint_magic != @endpoint.magic@");
+)~~~");
 #endif
-        out() << "            return nullptr;";
-        out() << "        }";
-        out() << "        i32 message_id = 0;";
-        out() << "        stream >> message_id;";
-        out() << "        if (stream.handle_any_error()) {";
+        generator.append_pattern(R"~~~(
+            return nullptr;
+        }
+
+        i32 message_id = 0;
+        stream >> message_id;
+        if (stream.handle_any_error()) {
+)~~~");
 #ifdef GENERATE_DEBUG_CODE
-        out() << "            dbg() << \"Failed to read message ID\";";
+        generator.append_pattern(R"~~~(
+            dbgln("Failed to read message ID");
+)~~~");
 #endif
-        out() << "            return nullptr;";
-        out() << "        }";
-        out() << "        OwnPtr<IPC::Message> message;";
-        out() << "        switch (message_id) {";
+        generator.append_pattern(R"~~~(
+            return nullptr;
+        }
+
+        OwnPtr<IPC::Message> message;
+        switch (message_id) {
+)~~~");
+
         for (auto& message : endpoint.messages) {
             auto do_decode_message = [&](const String& name) {
-                out() << "        case (int)Messages::" << endpoint.name << "::MessageID::" << name << ":";
-                out() << "            message = Messages::" << endpoint.name << "::" << name << "::decode(stream, size_in_bytes);";
-                out() << "            break;";
+                generator.set("message.name", name);
+
+                generator.append_pattern(R"~~~(
+        case (int)Messages::@endpoint.name@::MessageID::@message.name@:
+            message = Messages::@endpoint.name@::@message.name@::decode(stream, size_in_bytes);
+            break;
+)~~~");
             };
+
             do_decode_message(message.name);
             if (message.is_synchronous)
                 do_decode_message(message.response_name());
         }
-        out() << "        default:";
-#ifdef GENERATE_DEBUG_CODE
-        out() << "            dbg() << \"Failed to decode " << endpoint.name << ".(\" << message_id << \")\";";
-#endif
-        out() << "            return nullptr;";
 
-        out() << "        }";
-        out() << "        if (stream.handle_any_error()) {";
+        generator.append_pattern(R"~~~(
+        default:
+)~~~");
 #ifdef GENERATE_DEBUG_CODE
-        out() << "            dbg() << \"Failed to read the message\";";
+        generator.append_pattern(R"~~~(
+            dbgln("Failed to decode @endpoint.name@.({})", message_id);
+)~~~");
 #endif
-        out() << "            return nullptr;";
-        out() << "        }";
-        out() << "        return message;";
-        out() << "    }";
-        out();
-        out() << "    virtual OwnPtr<IPC::Message> handle(const IPC::Message& message) override";
-        out() << "    {";
-        out() << "        switch (message.message_id()) {";
+        generator.append_pattern(R"~~~(
+            return nullptr;
+        }
+
+        if (stream.handle_any_error()) {
+)~~~");
+#ifdef GENERATE_DEBUG_CODE
+        generator.append_pattern(R"~~~(
+            dbgln("Failed to read the message");
+)~~~");
+#endif
+        generator.append_pattern(R"~~~(
+            return nullptr;
+        }
+
+        return message;
+    }
+
+    virtual OwnPtr<IPC::Message> handle(const IPC::Message& message) override
+    {
+        switch (message.message_id()) {
+)~~~");
         for (auto& message : endpoint.messages) {
             auto do_decode_message = [&](const String& name, bool returns_something) {
+                generator.set("message.name", name);
+                generator.append_pattern(R"~~~(
+    FIXME FIXME FIXME
+)~~~");
+
                 out() << "        case (int)Messages::" << endpoint.name << "::MessageID::" << name << ":";
                 if (returns_something) {
                     out() << "            return handle(static_cast<const Messages::" << endpoint.name << "::" << name << "&>(message));";
