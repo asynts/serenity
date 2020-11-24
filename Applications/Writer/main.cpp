0e5e9ae7ce372ccd1175291d30249b6f85f3a343
asynts <asynts@gmail.com>
2020-11-24 18:50:19 +0100

    [FIXUP] I don't understand this crash.

--- Applications/Writer/main.cpp
+++ Applications/Writer/main.cpp
@@ -53,7 +53,8 @@ int main(int argc, char** argv)
     window->resize(570, 500);
 
     auto& widget = window->set_main_widget<GUI::Widget>();
-    dbgln("{}", main_window_ui_json);
+
+    // FIXME: For some reason this calls the load_from_json method on Writer::Node?!
     widget.load_from_json(main_window_ui_json);
 
     auto& writer = static_cast<Writer::WriterWidget&>(*widget.find_descendant_by_name("writer"));
