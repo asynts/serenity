a74d1f688a70a59d1f9f17e10d0f08af41edb5aa
asynts <asynts@gmail.com>
2021-01-24 00:21:39 +0100

    [FIXUP] constexpr->macro.

--- Kernel/Syscalls/select.cpp
+++ Kernel/Syscalls/select.cpp
@@ -95,7 +95,7 @@ int Process::sys$select(const Syscall::SC_select_params* user_params)
         fds.append(fd);
     }
 
-    if constexpr (IO_DEBUG || debug_poll_select)
+    if constexpr (IO_DEBUG || POLL_SELECT_DEBUG)
         dbgln("selecting on {} fds, timeout={}", fds_info.size(), params.timeout);
 
     if (current_thread->block<Thread::SelectBlocker>(timeout, fds_info).was_interrupted()) {
@@ -198,7 +198,7 @@ int Process::sys$poll(Userspace<const Syscall::SC_poll_params*> user_params)
             current_thread->update_signal_mask(previous_signal_mask);
     });
 
-    if constexpr (IO_DEBUG || debug_poll_select)
+    if constexpr (IO_DEBUG || POLL_SELECT_DEBUG)
         dbgln("polling on {} fds, timeout={}", fds_info.size(), params.timeout);
 
     if (current_thread->block<Thread::SelectBlocker>(timeout, fds_info).was_interrupted())
